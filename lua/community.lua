d89935f76c417e21d2e0dc0cee9c10c6d9296bb0
Andry Rakotozafindrabe <arakotoz@cern.ch>
2024-09-17 16:23:24 +0200

    add astrocommunity extensions

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,28 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- pack
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  -- bars-and-lines
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.everforest" },
+  -- completion
+  { import = "astrocommunity.completion.coq_nvim" },
+  -- editing support
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  -- git
+  { import = "astrocommunity.git.diffview-nvim" },
+  -- markdown and latex
+  { import = "astrocommunity.markdown-and-latex.markview-nvim" },
+  -- recipes
+  { import = "astrocommunity.recipes.neovide" },
 }
