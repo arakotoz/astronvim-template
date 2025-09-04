b568269ef8356def2c6c7783a23093e9b818c00d
Andry Rakotozafindrabe <arakotoz@cern.ch>
2025-09-04 16:50:56 +0200

    autosave session

--- lua/plugins/astrocore_sessions.lua
+++ lua/plugins/astrocore_sessions.lua
@@ -14,8 +14,17 @@ return {
   ---@type AstroCoreOpts
   opts = {
     sessions = {
-      -- disable the auto-saving of directory sessions
-      autosave = { cwd = false },
+      -- Configure auto saving
+      autosave = {
+        last = true, -- auto save last session
+        cwd = true, -- auto save session for each working directory
+      },
+      -- Patterns to ignore when saving sessions
+      ignore = {
+        dirs = {}, -- working directories to ignore sessions in
+        filetypes = { "gitcommit", "gitrebase" }, -- filetypes to ignore sessions
+        buftypes = {}, -- buffer types to ignore sessions
+      },
     },
     mappings = {
       n = {
